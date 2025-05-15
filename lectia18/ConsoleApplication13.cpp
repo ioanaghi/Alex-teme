0c1c6b31d5de666ea974665da73b54c702acf9fe
AlexChelcea <alex.chelcea79@gmail.com>
2025-05-15 17:23:35 +0300

    Add files via upload

--- lectia18/ConsoleApplication13.cpp
+++ lectia18/ConsoleApplication13.cpp
@@ -1,37 +1,22 @@
 #include <iostream>
 using namespace std;
-
-void descPrim(int n) {
-    int d = 2;
-    bool ok = 0;
-    int nr;
-    while (n != 1) {
-        nr = 0;
-        while (n % d == 0) {
-            nr++;
-            n = n / d;
-        }
-        if (nr >= 1 && ok == 1) {
-            cout << "*";
-        }
-        if (nr > 1) {
-            ok = 1;
-            cout << d << "^" << nr;
-        }
-        else
-            if (nr == 1) {
-                cout << d;
-                ok = 1;
-            }
-        if (d == 2)
-            d++;
-        else
-            d = d + 2;
+bool nrPerfect(int n) {
+    int S = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0)
+            S = S + i;
     }
+    if (n == S)
+        return 1;
+    else
+        return 0;
 }
 int main() {
     int n;
     cin >> n;
-    descPrim(n);
+    if (nrPerfect(n) == 1)
+        cout << "Da";
+    else
+        cout << "NU";
     return 0;
 }
