a353d632a4345458ee59ce622aa296015aa546de
Ioana Ghinet <163131805+ioanaghi@users.noreply.github.com>
2025-07-28 18:39:53 +0300

    Update Pb1.c

--- lectia24/Pb1.c
+++ lectia24/Pb1.c
@@ -4,13 +4,18 @@ using namespace std;
 
 int main()
 {
-	char v[101];
-	cin.getline(v, 101);
-	cout << v << endl;
-	cout << strlen(v);
-	if (strchr(v, "b") != NULL)
-		cout << "b este in sir";
-	else
-		cout << "b nu este in sir";
-}
+    char v[101];
+    cin.getline(v, 101);
+    cout << v << endl;
+    cout << strlen(v)<<endl;\
 
+    if (strchr(v, 'b') != NULL)
+        cout << "b este in sir";
+    else
+        cout << "b nu este in sir";
+
+    cout << endl;
+    cout<<(char)toupper(v[2])<<endl;
+    cout<<(int)'R'<<endl;
+    return 0;
+}
