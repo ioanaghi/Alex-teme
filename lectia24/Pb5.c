ffec8f944591e2bd88c76dff2af26e8210d9c325
Ioana Ghinet <163131805+ioanaghi@users.noreply.github.com>
2025-07-28 19:44:30 +0300

    Update Pb5.c

--- lectia24/Pb5.c
+++ lectia24/Pb5.c
@@ -3,27 +3,27 @@
 using namespace std;
 int main()
 {
-	char v[101];
-	char cuvant[101];
-	cin.getline(v, 101);
-	int ok = 0;
-	int j = 0;
-	for (int i = 0; i < strlen(v); i++) {
-		if (v[i] == ' ') {
-			if (ok == 1)
-				cout << cuvant << " ";
-			strcpy(cuvant, "");
-			ok = 0;
-			j = 0;
-		}
-		else {
-			if (strchr("aeiouAEIOU", v[i]) != NULL)
-				ok = 1;
-			cuvant[j] = v[i];
-			j++;
-		}
-	}
-	return 0;
-			
-}
+    char v[101];
+    char cuvant[101];
+    cin.getline(v, 101);
+    int ok = 0;
+    int j = 0;
+    for (int i = 0; i <= strlen(v); i++) {
+        if (v[i] == ' '||v[i]=='\0') {
+            cuvant[j]='\0';
+            if (ok == 1)
+                cout << cuvant << " ";
+            strcpy(cuvant, "");
+            ok = 0;
+            j = 0;
+        }
+        else {
+            if (strchr("aeiouAEIOU", v[i]) != NULL)
+                ok = 1;
+            cuvant[j] = v[i];
+            j++;
+        }
+    }
+    return 0;
 
+}
