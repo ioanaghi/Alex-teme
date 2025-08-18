a9c031b49cdd34a9e2653a0ed2adb90707b0a22b
AlexChelcea <alex.chelcea79@gmail.com>
2025-08-18 16:40:03 +0300

    Create Pb1.c

---
+++ lectia25/Pb1.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+    cout << X + Y << endl;
+    cout << X - Y << endl;
+    cout << X * Y << endl;
+    cout << X / Y << endl;
+    cout << X % Y;
+    return 0;
+}
