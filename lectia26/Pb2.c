4740230025024e491c33a7d6d8feb415dbd3a512
AlexChelcea <alex.chelcea79@gmail.com>
2025-09-03 17:39:38 +0300

    Create Pb2.c

---
+++ lectia26/Pb2.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char n[101];
+    cin.getline(n, 101);
+    cout << n << endl;
+    cout << strlen(n);
+    return 0;
+}
+
